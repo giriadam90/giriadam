5af15610f4af4bd1e3c2013c8608a03dc40886fe
giriadam90 <giriadam75@gmail.com>
2020-01-14 23:44:28 +0700

    commit pertama

---
+++ asset/js/typewriter-custom.js
@@ -0,0 +1,8 @@
+/* Type Writter Effect */
+/* Ref : https://safi.me.uk/typewriterjs*/
+const instance = new Typewriter('#typewriter', {
+  strings: ['Bootstrap 4', 'HTML 5', 'CSS 3',],
+  autoStart: true,
+  loop: true,
+});
+
